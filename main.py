2a1aca86210a1e27f343136a8e6ef620122c7fbc
Andrew Moore <Andrew.Moore@navihealth.com>
2020-08-20 16:01:47 -0500

    no button

--- main.py
+++ main.py
@@ -4,13 +4,12 @@ from playsound import playsound
 from PIL import ImageTk, Image
 
 def talk():
-    while(True):
-        playsound("annoying.mp3", block=False)
+    playsound("how-about-no.mp3")
 
 root = Tk()
-root.title("Easy Button")
+root.title("No Button")
 
-photo = ImageTk.PhotoImage(Image.open("jpgbutton.jpg"))
+photo = ImageTk.PhotoImage(Image.open("nobutton.jpeg"))
 
 button = ttk.Button(root, command=talk)
 button.config(image=photo)
